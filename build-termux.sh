42b2bc0f0a711057ea3d70206bf0dc05cfeffd03
doudeaboe <71425565+XxFrAnCoGoLxX@users.noreply.github.com>
2023-10-23 04:43:03 +0000

    Update build-termux.sh

--- build-termux.sh
+++ build-termux.sh
@@ -14,47 +14,47 @@ ask() {
 				return 1
 			fi
 		fi
-		pr "Asking again..."
+		pr "Preguntando de nuevo..."
 	done
 	return 1
 }
 
 if [ ! -f ~/.rvmm_"$(date '+%Y%m')" ]; then
-	pr "Setting up environment..."
+	pr "Preparando entorno..."
 	yes "" | pkg update -y && pkg install -y openssl git wget jq openjdk-17 zip
 	: >~/.rvmm_"$(date '+%Y%m')"
 fi
 
 if [ -f build.sh ]; then cd ..; fi
 if [ -d revanced-magisk-module ]; then
-	pr "Checking for revanced-magisk-module updates"
+	pr "Revisando actualizaciones de revanced-magisk-module"
 	git -C revanced-magisk-module fetch
 	if git -C revanced-magisk-module status | grep -q 'is behind'; then
 		pr "revanced-magisk-module already is not synced with upstream."
-		pr "Cloning revanced-magisk-module. config.toml will be preserved."
+		pr "Clonando revanced-magisk-module. config.toml se va a preservar."
 		cp -f revanced-magisk-module/config.toml .
 		rm -rf revanced-magisk-module
 		git clone https://github.com/j-hc/revanced-magisk-module --recurse --depth 1
 		mv -f config.toml revanced-magisk-module/config.toml
 	fi
 else
-	pr "Cloning revanced-magisk-module."
+	pr "Clonando revanced-magisk-module."
 	git clone https://github.com/j-hc/revanced-magisk-module --recurse --depth 1
 	sed -i '/^enabled.*/d; /^\[.*\]/a enabled = false' revanced-magisk-module/config.toml
 fi
 cd revanced-magisk-module
 chmod +x build.sh build-termux.sh
 
-if ask "Do you want to open the config.toml for customizations? [y/n]"; then
+if ask "Querés abrir config.toml para personalizar? [y/n]"; then
 	nano config.toml
 fi
-if ! ask "Setup is done. Do you want to start building? [y/n]"; then
+if ! ask "Preparación terminada. Empezar? [y/n]"; then
 	exit 0
 fi
 ./build.sh
 
 cd build
-pr "Ask for storage permission"
+pr "Pidiendo permiso de almacenamiento"
 until
 	yes | termux-setup-storage >/dev/null 2>&1
 	ls /sdcard >/dev/null 2>&1
@@ -69,7 +69,7 @@ for op in *; do
 	mv -f "${PWD}/${op}" ~/storage/downloads/revanced-magisk-module/"${op}"
 done
 
-pr "Outputs are available in /sdcard/Download/revanced-magisk-module folder"
+pr "Los archivos finales se encuentran en /sdcard/Download/revanced-magisk-module "
 am start -a android.intent.action.VIEW -d file:///sdcard/Download/revanced-magisk-module -t resource/folder
 sleep 2
 am start -a android.intent.action.VIEW -d file:///sdcard/Download/revanced-magisk-module -t resource/folder
